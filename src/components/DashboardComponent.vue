8b672e8e64aa28bbacbe87d7182b744b0b36cde4
Josh <126632428+SSKiyan25@users.noreply.github.com>
2024-02-19 02:51:39 +0800

    Update DashboardComponent.vue

--- src/components/DashboardComponent.vue
+++ src/components/DashboardComponent.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="flex flex-col h-screen">
-    
+  <div class="flex flex-col items-center justify-center h-screen pt-12 pb-24 box-border">
+    <div class="flex-auto rounded-lg shadow-2xl w-5/6">
+      <div class="flex items-center justify-between px-8 py-8 mt-4 border-b-2">
+        <h4 class="text-2xl font-bold dark:text-black">FlashCards</h4>
+        <button type="button" class="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">+ Add</button>
+      </div>
+    </div>
   </div>
 </template>
